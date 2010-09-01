9a203a1ffcdb4adce4dde548d2beaab383e1ff3d
Kurt Raschke <kurt@kurtraschke.com>
2010-09-01 02:03:43 -0400

    Add support for several additional formats.

--- geolucidate/parser.py
+++ geolucidate/parser.py
@@ -7,18 +7,18 @@ parser_re = re.compile(u"""\\b
     (?P<latdir>NORTH|SOUTH|[NS])?
     # Latitude degrees: two digits 0-90
     (?P<latdeg>([0-8][0-9])|90)
-    # Optional space or degree mark separating degrees and minutes
-    (\ |(?P<degmark>\ ?(º|°)\ ?|\ DEGREES,\ ))?
+    # Optional space, degree mark, period, or word separating degrees and minutes
+    (\ |(?P<degmark>\ ?(º|°)\ ?|\.|-|\ DEGREES,\ ))?
     (?P<latminsec>
     # Latitude minutes: two digits 0-59
     (?P<latmin>[0-5][0-9])
     # If there was a degree mark before, look for punctuation after the minutes
-    (?(degmark)(\'|\ MINUTES(,\ )?))?
+    (?(degmark)(\'|"|\.|\ MINUTES(,\ )?))?
     (
     # Latitude seconds: two digits 0-59
-    (?P<latsec>([0-9]|[0-5][0-9]))(?(degmark)("|\ SECONDS\ ))|
+    (?P<latsec>([0-9]|[0-5][0-9]))(?(degmark)("|'|\ SECONDS\ )?)|
     # Decimal fraction of minutes
-    (?P<latdecsec>\\.\\d{1,2}))?
+    (?P<latdecsec>\\.\\d{1,3}))?
     )?
     # Latitude direction, second position, optionally preceded by a space
     (\ ?(?P<latdir2>(?(latdir)|(NORTH|SOUTH|[NS]))))
@@ -29,17 +29,17 @@ parser_re = re.compile(u"""\\b
     # Longitude degrees: two or three digits
     (?P<longdeg>\\d{2,3}?)
     # If there was a degree mark before, look for another one here
-    (\ |(?(degmark)(\ ?(º|°)\ ?|\ DEGREES,\ )))?
+    (\ |(?(degmark)(\ ?(º|°)\ ?|\.|-|\ DEGREES,\ )))?
     (?(latminsec)   #Only look for minutes and seconds in the longitude
     (?P<longminsec> #if they were there in the latitude
     # Longitude minutes: two digits
     (?P<longmin>[0-5][0-9])
     # If there was a degree mark before, look for punctuation after the minutes
-    (?(degmark)('|\ MINUTES(,\ )?))?
+    (?(degmark)('|"|\.|\ MINUTES(,\ )?))?
     # Longitude seconds: two digits 0-59
-    ((?P<longsec>([0-5][0-9]))(?(degmark)("|\ SECONDS\ ))|
+    ((?P<longsec>([0-9][0-9]))(?(degmark)("|'|\ SECONDS\ )?)|
     # Decimal fraction of minutes
-    (?P<longdecsec>\\.\\d{1,2}))?
+    (?P<longdecsec>\\.\\d{1,3}))?
     ))
     #Longitude direction, second position: optionally preceded by a space
     (?(latdir)|\ ?(?P<longdir2>(EAST|WEST|[EW])))
