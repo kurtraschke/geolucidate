bf24c6176452bd058c28044527e194445fe80f73
Kurt Raschke <kurt@kurtraschke.com>
2011-01-25 19:55:52 -0500

    Update setup.py.

--- setup.py
+++ setup.py
@@ -7,6 +7,19 @@ setup(
     author='Kurt Raschke',
     author_email='kurt@kurtraschke.com',
     url='http://github.com/kurtraschke/geolucidate',
+    license = "MIT",
+    
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Internet",
+        "Topic :: Text Processing :: Filters",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Scientific/Engineering :: GIS"
+        ],
+
     packages=[
         "geolucidate",
         "geolucidate.links",
