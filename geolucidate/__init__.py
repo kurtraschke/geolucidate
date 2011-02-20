d037ca29a64811e143a43bd03fe48ee3af7db6a7
Kurt Raschke <kurt@kurtraschke.com>
2011-02-20 17:19:06 -0500

    Fix doctest in __init__.py to make it more realistic.

--- geolucidate/__init__.py
+++ geolucidate/__init__.py
@@ -3,9 +3,10 @@
 geographic coordinates in text.  For most purposes, calling
 :func:`~.replace` with the text to be processed will suffice.
 
+
+>>> from geolucidate import replace
 >>> replace("58147N/07720W")
 u'<a href="http://maps.google.com/maps?q=58.235278%2C-77.333333+%2858147N%2F07720W%29&ll=58.235278%2C-77.333333&t=h" title="58147N/07720W (58.235278, -77.333333)">58147N/07720W</a>'
-
 
 """
 
